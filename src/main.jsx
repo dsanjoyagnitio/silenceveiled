26c742ff1e68474b66eccc01ec170015fd9699b1
dsanjoyagnitio <151919213+dsanjoyagnitio@users.noreply.github.com>
2025-10-09 12:36:58 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
-import { createRoot } from 'react-dom/client';
-import { HashRouter } from "react-router-dom";
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
-)
+<BrowserRouter basename="/silenceveiled">
+  <App />
+</BrowserRouter>
