2501a9290e60606de4d20168a7b65da806e82335
dsanjoyagnitio <151919213+dsanjoyagnitio@users.noreply.github.com>
2025-10-09 12:41:35 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css"; // optional
 
-<BrowserRouter basename="/silenceveiled">
-  <App />
-</BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
